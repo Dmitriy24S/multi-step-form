93ce8453c3884933409d4add237509f72be6fee9
Dmitriy24S <devilins24@gmail.com>
2023-09-11 17:43:50 +0300

    test: TextField shows validation message

--- src/components/shared/TextField/TextField.test.tsx
+++ src/components/shared/TextField/TextField.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import type { FieldError } from 'react-hook-form'
 import { useForm, useController } from 'react-hook-form'
 
 import { TextField } from './TextField'
@@ -27,5 +28,44 @@ test('renders TextField component with react hook form controller', () => {
   }
 
   render(<Component />)
+
+  const input = screen.getByPlaceholderText('test placeholder')
+
+  expect(input).toBeInTheDocument()
+
+  // screen.debug()
+})
+
+test('TextField shows validation error message', async () => {
+  const Component = () => {
+    const { control } = useForm<{
+      test: string
+      test1: { test: string }[]
+    }>()
+
+    useController({
+      control,
+      defaultValue: '',
+      name: 'test',
+    })
+
+    return (
+      <TextField
+        name="test"
+        control={control}
+        errors={{
+          test: {
+            message: 'validation error message',
+          } as FieldError,
+        }}
+        placeholder="test placeholder"
+      />
+    )
+  }
+
+  render(<Component />)
+
+  expect(screen.getByText('validation error message')).toBeInTheDocument()
+
   // screen.debug()
 })
