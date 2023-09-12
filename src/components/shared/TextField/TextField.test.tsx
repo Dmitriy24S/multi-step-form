c18668ede40a3780fd99d1a3a87db161d3b6d403
Dmitriy24S <devilins24@gmail.com>
2023-09-12 13:55:20 +0300

    test: TextField updates input value

--- src/components/shared/TextField/TextField.test.tsx
+++ src/components/shared/TextField/TextField.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import type { FieldError } from 'react-hook-form'
 import { useForm, useController } from 'react-hook-form'
 
@@ -68,4 +69,47 @@ test('TextField shows validation error message', async () => {
   expect(screen.getByText('validation error message')).toBeInTheDocument()
 
   // screen.debug()
+})
+
+test('TextField updates input value', async () => {
+  const Component = () => {
+    const { control } = useForm<{
+      test: string
+      test1: { test: string }[]
+    }>()
+
+    useController({
+      control,
+      defaultValue: '',
+      name: 'test',
+    })
+
+    return (
+      <TextField
+        name="test"
+        control={control}
+        errors={{}}
+        placeholder="test placeholder"
+      />
+    )
+  }
+
+  render(<Component />)
+
+  const input = screen.getByPlaceholderText(
+    'test placeholder',
+  ) as HTMLInputElement
+
+  const inputValue = input.value
+
+  expect(inputValue).toBe('')
+
+  userEvent.type(input, 'hello world')
+
+  await waitFor(() => {
+    const updatedInputValue = input.value
+    expect(updatedInputValue).toBe('hello world')
+  })
+
+  screen.debug()
 })
